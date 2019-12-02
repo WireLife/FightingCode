12aa7692b4f8e58e4d8ba43acd72269d6dd9d581
asd <58359940+dhasuhdai@users.noreply.github.com>
2019-12-02 15:37:43 +0800

    Update 9.1.cpp

--- 2020备战蓝桥/王浩杰/第九次作业/9.1.cpp
+++ 2020备战蓝桥/王浩杰/第九次作业/9.1.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+#include<cstdlib>
+#include<cstdio>
+using namespace std;
+char a[100];
 
+double f()
+{
+	float num = 0, i, s, sum = 0;
+	cin >> a;
+	if (a[0] == '*') { return f()*f(); }
+	else if (a[0] == '+') { return f() + f(); }
+	else if (a[0] == '-') { return f() - f(); }
+	else if (a [0]== '/') { return f() / f(); }
+	else return atof(a);
+	return num;
+}
+int main()
+{
+	double a;
+	a = f();
+	cout << a;
+	system("pause");
+	return 0;
+}
