762214c85c1374a3c0bdf1606174d5e204865811
kkyyo-123 <55020314+kkyyo-123@users.noreply.github.com>
2019-12-06 19:54:54 +0800

    Create 9-5.cpp

---
+++ 2020备战蓝桥/刘燚杰/9-5.cpp
@@ -0,0 +1,31 @@
+//运行不出来
+#include<iostream>
+using namespace std;
+
+int apple(int a,int b)
+{
+    if(a == 1 ||b == 1)//一个苹果或一个盘子都只有一种方法
+        return 1;
+    if(a>b)
+        return apple(b,b);
+    else
+        return apple(a,b-1)+apple(a-b,b);
+}
+int main()
+{
+    int t = 0;//测试数据组数
+    cin>>t;
+    int time = 0;//次数
+    int m = 0;//苹果
+    int n = 0;//盘子
+    int o[20]={0};
+    while(time !=t )
+    {
+        cin>>m>>n;
+        o[time] = apple(m,n);
+        time++;
+    }
+    for(int i=0;i<t;i++)
+        cout<<o[i]<<endl;
+    return 0;
+}
