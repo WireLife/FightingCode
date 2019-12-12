e9a31ef48fc9396995eeea790fe80c503b6c2aa4
HYK971277 <58360107+HYK971277@users.noreply.github.com>
2019-12-12 16:02:42 +0800

    Update 1.cpp

--- 2020备战蓝桥/何永康/第十次作业/1.cpp
+++ 2020备战蓝桥/何永康/第十次作业/1.cpp
@@ -1,1 +1,29 @@
 
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+	char a[14];
+	cin >> a;
+	int b[10];
+	int j = 0;
+	int sum = 0;
+	for (int i = 0; i < 13; i++)
+	{
+		if (a[i] != '-')
+		{
+			b[j++] = a[i] - '0';
+		}
+	}
+	for (int i = 0; i < 9; i++)
+		sum = sum + (i + 1) * b[i];
+	if (sum % 11 == b[9])
+		cout << "YES";
+	else
+		cout << "NO";
+
+
+	return 0;
+}
