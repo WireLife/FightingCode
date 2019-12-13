b354f01e5ea8e9a0dfe1361ca11bb9ecdad7155c
kkyyo-123 <55020314+kkyyo-123@users.noreply.github.com>
2019-12-13 19:09:53 +0800

    Update 9-1.cpp

--- 2020备战蓝桥/刘燚杰/第九次作业/9-1.cpp
+++ 2020备战蓝桥/刘燚杰/第九次作业/9-1.cpp
@@ -1,1 +1,86 @@
 
+#include<iostream>
+#include<string>
+#include<sstream>//isstringstream
+#include<stdio.h>
+using namespace std;
+
+int main()
+{
+    string s = "* 11 12";
+    int former = 0,midder=0,latter = 0;
+    istringstream iss(s);
+    string subs;
+    iss >> subs;
+    if(subs == "*" || subs == "/")
+  {
+    if(subs == "*")
+        {
+            iss >> subs;
+            if(subs != "+" && subs != "-")
+            {
+                former = atoi(subs.c_str());
+                iss >> subs;
+                latter = atoi(subs.c_str());
+             //   return (former*latter);
+                    cout<<(former*latter)<<endl;
+            }
+            else if(subs == "+")
+            {
+                iss>>subs;
+                former = atoi(subs.c_str());
+                iss >> subs;
+                midder = atoi(subs.c_str());
+                iss >> subs;
+                latter = atoi(subs.c_str());
+            //    return ((former+midder)*latter);
+                cout<<((former+midder)*latter)<<endl;
+            }
+            else if(subs == "-")
+            {
+                iss>>subs;
+                former = atoi(subs.c_str());
+                iss >> subs;
+                midder = atoi(subs.c_str());
+                iss >> subs;
+                latter = atoi(subs.c_str());
+            //    return ((former-midder)*latter);
+                cout<<((former-midder)*latter)<<endl;    
+            }
+        }
+        if(subs == "/")
+        {
+            iss >> subs;
+            if(subs != "+" && subs != "-")
+            {
+                int former = atoi(subs.c_str());
+                iss >> subs;
+                int latter = atoi(subs.c_str());
+            //    return (former/latter);
+                cout<<(former/latter)<<endl;
+            }
+            else if(subs == "+")
+            {
+                iss>>subs;
+                former = atoi(subs.c_str());
+                iss >> subs;
+                midder = atoi(subs.c_str());
+                iss >> subs;
+                latter = atoi(subs.c_str());
+            //    return ((former+midder)/latter);
+                cout<<((former+midder)/latter)<<endl;
+            }
+            else if(subs == "-")
+            {
+                iss>>subs;
+                former = atoi(subs.c_str());
+                iss >> subs;
+                midder = atoi(subs.c_str());
+                iss >> subs;
+                latter = atoi(subs.c_str());
+            //    return ((former-midder)/latter);  
+                cout<<((former-midder)/latter)<<endl;  
+            }
+        }
+    }
+}
