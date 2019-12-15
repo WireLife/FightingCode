8036b1142a87771cdc063b45141a1b9cfe2ac045
FireLife <57707327+WireLife@users.noreply.github.com>
2019-12-15 16:58:46 +0800

    Add files via upload

---
+++ 2020备战蓝桥/刘震/第十一次作业/11.5 设有语句.cpp
@@ -0,0 +1,19 @@
+/*11.5 …Ë”–”Ôæ‰*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	float x, y;
+	char ch1, ch2, ch3;
+	cin >> a >> b >> x >> y >> ch1;
+	ch2 = cin.get();
+	cin >> ch3;
+
+
+	cout << a << ' ' << b << ' ' << x << ' ' << ' ' << y << ' ' << endl;
+	cout << "ch1:" << ch1 << '\n' << "ch2:" << ch2 << '\n' <<"ch3:"<< ch3 << endl;
+	system("pause");
+	return 0;
+}
