0598f064d48103617a87953e0a932185a7369578
Nancyfeerry <45453086+Nancyfeerry@users.noreply.github.com>
2019-12-20 19:30:32 +0800

    第十一作业

---
+++ 2020备战蓝桥/张宇菲/张宇菲/11.5.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	int cnt=0;
+	string str;
+	cin>>str;
+	for(int i=0;i<=str.length();i++)
+	{
+		if(str[i]>='0'&&str[i]<='9')
+			cnt++;
+	}
+	cout<<cnt<<endl;
+	return 0;
+}
---
+++ 2020备战蓝桥/张宇菲/张宇菲/11.6.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c,d;
+	cin>>a>>b>>c>>d;
+	cout<<a<<'\t'<<hex<<b<<'\t'<<oct<<c<<endl;
+	return 0;
+}
