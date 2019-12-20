068ffa56d0a028b04ec0ac53a9b2e752bf80812a
Alice-Liushiyu <58360177+Alice-Liushiyu@users.noreply.github.com>
2019-12-20 19:18:48 +0800

    Add files via upload

---
+++ 2020备战蓝桥/刘诗雨-/第十一次作业/11.2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<string.h>
+using namespace std; 
+int main() {
+//	std::string s1,s2;
+//	std::cin>>s1;
+//	std::cin>>s2;
+    char s1,s2;
+    cin>>s1;
+    cin>>s2;
+	strcat(s1,s2);
+//	cout<<(s1+s2)<<endl;
+	return 0;
+}
---
+++ 2020备战蓝桥/刘诗雨-/第十一次作业/11.3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include<string.h>
+using namespace std; 
+int main() {
+	std::string str;
+	std::cin>>str;
+	cout<<str<<endl;
+	return 0;
+}
