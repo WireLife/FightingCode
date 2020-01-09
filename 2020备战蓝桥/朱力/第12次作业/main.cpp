0f1c79259777772685dc3c79168f6248de151c92
Bilibili-toast <58360147+Bilibili-toast@users.noreply.github.com>
2020-01-09 21:37:32 +0800

    Add files via upload

--- 2020备战蓝桥/朱力/第12次作业/main.cpp
+++ 2020备战蓝桥/朱力/第12次作业/main.cpp
@@ -1,36 +1,28 @@
-//12.1
-#include<bits/stdc++.h>
-//参考网上没懂题目意思
+#include<stdio.h>
+#include<iostream>
+#include <vector>
+#include<algorithm>
 using namespace std;
-int Max[200];
-vector<string> s[1010];
-int read(){
-    int x=0,f=1;char ch=getchar();
-    for(;ch<'0'||ch>'9';ch=getchar())if(ch=='-')f=-1;
-    for(;ch>='0'&&ch<='9';ch=getchar())x=(x<<1)+(x<<3)+ch-'0';
-    return x*f;
-}
-int main(){
-    int n=0;string ss,buf;
-    while(getline(cin,ss)){
-        int t=0;
-        stringstream S(ss);
-        while(S>>buf){
-            Max[t]=max((int)buf.length(),Max[t]);//将每一列的长度
-            //最长的单词大小保存下来
-            s[n].push_back(buf);//将当前的单词加入向量中储存
-            t++;
-        }
-        n++;
+int main()
+{
+    int middle;
+    vector<int> x;
+    int a,b;
+    int zu,n;
+    int sumx=0;
+    cin>>zu;
+    while(zu){
+        cin>>n;
+    while(n){
+        cin>>a>>b;
+        sumx=sumx+a;
+        x.push_back(a);
+        n--;
     }
-    cout<<setiosflags(ios::left);//将输出格式定义为左对齐输出
-    for(int i=0;i<n;i++){
-        int j=0;
-        for(j;j<s[i].size()-1;j++)
-            cout<<setw(Max[j]+1)<<s[i][j];//setw()将括号里的
-            //参数的大小
-            //作为输出的值占的位置大小
-        cout<<s[i][j]<<endl;
+    middle=sumx/n;
+    if(find(x.begin(),x.end(),middle))
+    zu--;
     }
-    return 0;
+    //算出平均值，按照大小排序，然后在vector里面首尾一一对应取平均值，看是否的关于
+    //总体均值相等;
 }
