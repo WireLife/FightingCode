6522d9af1b5dc43bc80e35027eefeacd4b92a636
Bilibili-toast <58360147+Bilibili-toast@users.noreply.github.com>
2020-01-09 09:06:40 +0800

    Add files via upload

---
+++ 2020备战蓝桥/朱力/第12次作业/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"请输入是几元组"<<endl;
+    cin>>n;
+    cout<<"请输入数据成员"<<endl;
+    int a[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    int k=1000;
+    int count=0;
+    while(k){
+        a[n]=abs(a[0]-a[n]);
+        for(int i=0;i<n-1;i++){
+            a[i]=abs(a[i+1]-a[i]);
+        }
+        for(int i=0;i<n;i++){
+            if(a[i]==0)
+                count++;
+        }
+        if(count==n)
+        {
+            cout<<"is zero!"<<endl;
+            return 0;
+        }
+        else
+        {
+            k--;
+            continue;
+        }
+    }
+    cout<<"is loop!"<<endl;
+    return 0;
+}
