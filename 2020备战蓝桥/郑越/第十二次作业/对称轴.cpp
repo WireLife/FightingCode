1135baff61c2d233b708c5557683945190ee3d24
Richard Zheng <57664875+Ibroad@users.noreply.github.com>
2020-01-10 16:46:01 +0800

    Update 对称轴.cpp

--- 2020备战蓝桥/郑越/第十二次作业/对称轴.cpp
+++ 2020备战蓝桥/郑越/第十二次作业/对称轴.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 using namespace std;
-
 int main(int argc, const char * argv[]) {
     int N,i,j,xnSym=0,xSym=0,m,x,xS,k,key;
     scanf("%d",&N);
     int co_ord[N][2];
-    
      for(i=0;i<N;i++)
         scanf("%d %d",&co_ord[i][0],&co_ord[i][1]);
     k=0;
@@ -20,26 +18,25 @@ int main(int argc, const char * argv[]) {
     xSym=xnSym/k;
     m=co_ord[0][1];
     key=1;
-        for(i=1;i<N;i++)
+    for(i=1;i<N;i++)
+    {
+        m=co_ord[i][1];
+        x=0;
+        k=0;
+        for(j=0;j<N;j++)
         {
-            m=co_ord[i][1];
-            x=0;
-            k=0;
-            for(j=0;j<N;j++)
+            if(co_ord[j][1]==m)
             {
-                
-                if(co_ord[j][1]==m)
-                {
-                    x=x+co_ord[j][0];
-                    k++;
-                }
+                x=x+co_ord[j][0];
+                k++;
             }
-            xS=x/k;
-            if(xS!=xSym)
-            {
-                key=0;
-            }break;
         }
+        xS=x/k;
+        if(xS!=xSym)
+        {
+            key=0;
+        }break;
+    }
     if(key==1)
         cout<<"Yes"<<endl;
     else
@@ -48,3 +45,4 @@ int main(int argc, const char * argv[]) {
         
     return 0;
 }
+
