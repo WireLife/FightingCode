38b20a7f5f13a1ed9581640195e3316afac48be0
bcf1 <58024399+bcf1@users.noreply.github.com>
2020-01-10 10:17:56 +0800

    第七题

---
+++ 2020备战蓝桥/周鑫鹏/第十二次作业/5_7.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int M(vector<int>::iterator it,vector<int>::iterator it1);
+int main()
+{
+    int a,t;
+    int k=0;
+    vector<int> vec;
+    cout<<"0 to end"<<endl;
+    cin>>t;
+    while(t!=0)
+    {
+        vec.push_back(t);
+        cin>>t;
+    }
+    cout<<"位置:";
+    cin>>a;
+    vector<int>::iterator it=vec.begin();
+    vector<int>::iterator b=it+a;
+    while(1)
+    {
+        if(M(it,vec.end()))
+        {
+            if(it==b)
+                break;
+            k++;
+        }
+        else
+        {
+            vec.push_back(*it);
+            if(it==b)
+                b=vec.end();
+        }
+        it++;
+    }
+    cout<<"完成时刻:"<<k<<endl;
+    return 0;
+}
+int M(vector<int>::iterator it,vector<int>::iterator it1)
+{
+    vector<int>::iterator itr=it;
+    auto a=max_element(it,it1);
+    if(*it==*a)
+        return 1;
+    return 0;
+}
