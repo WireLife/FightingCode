b2e16b5a6ceb6351e14ff7400eeb32ae72590a72
Bilibili-toast <58360147+Bilibili-toast@users.noreply.github.com>
2020-01-13 11:41:28 +0800

    Add files via upload

---
+++ 2020备战蓝桥/朱力/第十三次作业/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <map>
+//13.6
+using namespace std;
+
+int main()
+{
+    int sum;
+    int i;
+    map<int,int> ms;
+    int num,m,n;
+    int k,total=1;
+    cin>>num>>k;
+    int b=k;
+    while(b){
+        cin>>m>>n;
+        ms.insert(pair<int, int>(m,n));
+        b--;
+    }
+    cout<<endl;
+    for(i=0;i<k;i++){
+        total=total*2;
+    }
+    cout<<total<<endl;
+    return 0;
+}
